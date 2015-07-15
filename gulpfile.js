8a54d1a4f836cd147710f84abec580b7472e8e16
Brendan Zabarauskas <bjzaba@yahoo.com.au>
2015-07-15 18:56:41 +1000

    Separate clean tasks

--- gulpfile.js
+++ gulpfile.js
@@ -61,13 +61,9 @@ gulp.task('deploy', ['build', 'readme'], function() {
 
 // Cleaning
 
-gulp.task('clean', function(cb) {
-  del(['./tmp', './.publish'], cb);
-});
-
-gulp.task('nuke', ['clean'], function(cb) {
-  del(['./elm-stuff', './node_modules']);
-});
+gulp.task('clean', function(cb) { del(['./tmp', './.publish'], cb); });
+gulp.task('clean:elm', function(cb) { del(['./elm-stuff']); });
+gulp.task('clean:npm', function(cb) { del(['./elm-stuff']); });
 
 // Default
 
